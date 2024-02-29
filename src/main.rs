1e0c3236655189f0b50196717bcad638069a450a
KwaejenAepul <66973949+KwaejenAepul@users.noreply.github.com>
2024-02-29 11:52:02 +0100

    Small changes

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,17 @@ use macroquad::prelude::*;
 mod imageconverter;
 mod player;
 
-
 #[macroquad::main("silly lil guy")]
 async fn main() {
     let image = load_image("assets/lil_guy_sprites.png").await.unwrap();
     let spritevec = imageconverter::spritesheet_to_vec(image, 32.0, 12.0);
-    let mut player = player::Player {
-        speed: 500.0,
-        position: player::Position { x: 200.0, y: 200.0 },
-        direction: player::Direction::Down,
-        textures: spritevec,
-        animation_timer: 0.0,
-    };
+    let mut player = player::Player::create_player(
+        300.0,
+        player::Position { x: 200.0, y: 200.0 },
+        player::Direction::Down,
+        spritevec,
+        0.0,
+    );
 
     loop {
         clear_background(PURPLE);
--- src/player.rs
+++ src/player.rs
@@ -14,14 +14,28 @@ pub struct Position {
 }
 #[derive(Debug)]
 pub struct Player {
-    pub speed: f32,
-    pub position: Position,
-    //size: f32,
-    pub direction: Direction,
-    pub textures: Vec<Texture2D>,
-    pub animation_timer: f32,
+    speed: f32,
+    position: Position,
+    direction: Direction,
+    textures: Vec<Texture2D>,
+    animation_timer: f32,
 }
 impl Player {
+    pub fn create_player(
+        speed: f32,
+        position: Position,
+        direction: Direction,
+        texture: Vec<Texture2D>,
+        animation_timer: f32,
+    ) -> Player {
+        Player {
+            speed: speed,
+            position: position,
+            direction: direction,
+            textures: texture,
+            animation_timer: animation_timer,
+        }
+    }
     pub fn update(&mut self) {
         if is_key_down(KeyCode::Up) {
             self.direction = Direction::Up;
@@ -40,55 +54,49 @@ impl Player {
 
     pub fn draw(&mut self) {
         let mut image: &Texture2D;
-        match self.direction{
-            Direction::Up =>{image = &self.textures[9]},
-            Direction::Down => {image = &self.textures[0]},
-            Direction::Left => {image = &self.textures[3]},
-            Direction::Right => {image = &self.textures[6]},
+        match self.direction {
+            Direction::Up => image = &self.textures[9],
+            Direction::Down => image = &self.textures[0],
+            Direction::Left => image = &self.textures[3],
+            Direction::Right => image = &self.textures[6],
         }
         if is_key_down(KeyCode::Up) {
-            if self.animation_timer< 0.25{
+            if self.animation_timer < 0.25 {
                 image = &self.textures[10];
                 self.animation_timer += get_frame_time();
-            }else {
+            } else {
                 image = &self.textures[11];
                 self.animation_timer += get_frame_time();
             }
         } else if is_key_down(KeyCode::Down) {
-            if self.animation_timer< 0.25{
+            if self.animation_timer < 0.25 {
                 image = &self.textures[1];
-                self.animation_timer += get_frame_time(); 
-            }else {
+                self.animation_timer += get_frame_time();
+            } else {
                 image = &self.textures[2];
                 self.animation_timer += get_frame_time();
             }
         } else if is_key_down(KeyCode::Left) {
-            if self.animation_timer< 0.25{
+            if self.animation_timer < 0.25 {
                 image = &self.textures[4];
-                self.animation_timer += get_frame_time(); 
-            }else {
+                self.animation_timer += get_frame_time();
+            } else {
                 image = &self.textures[5];
                 self.animation_timer += get_frame_time();
             }
         } else if is_key_down(KeyCode::Right) {
-            if self.animation_timer< 0.25{
+            if self.animation_timer < 0.25 {
                 image = &self.textures[7];
-                self.animation_timer += get_frame_time(); 
-            }else {
+                self.animation_timer += get_frame_time();
+            } else {
                 image = &self.textures[8];
                 self.animation_timer += get_frame_time();
             }
         }
-        if self.animation_timer > 0.5{
+        if self.animation_timer > 0.5 {
             self.animation_timer = 0.0;
         }
 
-
-        draw_texture(
-            image,
-            self.position.x,
-            self.position.y,
-            WHITE,
-        );
+        draw_texture(image, self.position.x, self.position.y, WHITE);
     }
 }
